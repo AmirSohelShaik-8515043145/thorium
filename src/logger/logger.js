b2a7fbc22dc9ff94313ac3988fccfa89e499f4ec
AmirSohelShaik-8515043145 <amirsohelsk07@gmail.com>
2022-02-22 11:21:55 +0530

    assignment submission

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+function welcome(){
+    console.log("Welcome to my application. I am Amir Sohel Shaik and a part of FunctionUp Thorium cohort");
+    console.log("----------------------------------------")
+}
+
+module.exports = {welcome}

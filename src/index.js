6df038e34ddc637b42dc9e40fe7814ecc9f3d220
AmirSohelShaik-8515043145 <amirsohelsk07@gmail.com>
2022-04-03 10:22:44 +0530

    chamges

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//mongodb+srv://Pramod09:Pramod%4010@pramod09.i3td2.mongodb.net/pramod09-db
 
-// mongodb+srv://swethaHirge:eNbiwvH7LUDppBrx@cluster0.0xins.mongodb.net/group19-Database?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://swethaHirge:eNbiwvH7LUDppBrx@cluster0.0xins.mongodb.net/group19-Database?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://amir-thorium:NSE7ZdUlu4no9WRF@cluster0.gchuo.mongodb.net/group19-DataBase?authSource=admin&replicaSet=atlas-cw2o95-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
     useNewUrlParser: true
 })
     .then(() => console.log("MongoDb is connected"))
